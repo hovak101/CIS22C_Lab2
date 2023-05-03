a42cf04197b384c7f3b2eb7ed19919f53048109b
Alex Hovakimyan <hovak101@gmail.com>
2023-05-03 13:32:01 -0700

    Initial commit

---
+++ Lab 2/Krone.cpp
@@ -0,0 +1,13 @@
+#pragma once
+#include "Currency.cpp"
+#include <iostream>
+#include <string>
+
+class Krone : public Currency
+{
+private:
+	std::string name;
+
+public:
+
+};
---
+++ Lab 2/main.cpp
@@ -0,0 +1,50 @@
+#include <iostream>
+#include <string>
+#include <sstream>
+#include "Currency.cpp"
+#include "Krone.cpp"
+#include "Soum.cpp"
+
+using namespace std;
+
+int main() {
+	Currency* currencies[2];
+	//currencies[0] = new Soum(0);      //should they be dynamically allocated?
+	//currencies[1] = new Krone(0);
+
+
+	// what exceptions is he talking about? See below:
+	// "Throw String (or equivalent) exceptions from within the classes to ensure that invalid objects cannot be created."
+	// Can we assume input won't be invalid? NO! Have to handle it. 
+	// I think Krone and Soum are not supposed to be negative. \
+	// Should input errors result in just error messages or exceptions that halt the program? 
+
+
+	string line;
+	char modificationType;
+	char valueType;
+	double value;
+	string receiver;
+
+	cout << "Input: ";
+	getline(cin, line);
+	stringstream stream;
+
+	while (line != "q") {                   //Should I change so that it only looks for the character?
+		stream.str(line);
+		stream >> modificationType >> valueType >> value >> receiver;
+		cout << "Output: " << endl; 
+		cout << "modification type: " << modificationType << endl;
+		cout << "value type: " << valueType << endl;
+		cout << "value: " << value << endl;
+		cout << "receiver: " << receiver << endl << endl;
+
+		stream.clear();
+
+		cout << "Input: ";
+		getline(cin, line);
+	}
+
+
+	return 0;
+}
